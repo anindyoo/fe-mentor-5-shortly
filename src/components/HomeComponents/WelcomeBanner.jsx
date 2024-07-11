84278d991003f0178c0557bb82615aacda88423b
anindyoo <anindyo0216@gmail.com>
2024-07-11 18:07:42 +0700

    feat: add banner illustration.

--- src/components/HomeComponents/WelcomeBanner.jsx
+++ src/components/HomeComponents/WelcomeBanner.jsx
@@ -1,18 +1,24 @@
+import workingIllustration from '../../assets/images/illustrations/illustration-working.svg';
+
 const WelcomeBanner = () => {
   console.log();
 
   return (
     <section className="
     WELCOME-BANNER
-    border pt-[5.875rem] pb-[13.875rem]"
+    SECTION-CENTER
+    flex flex-row justify-start gap-[6.563rem]
+    w-full
+    pt-[5.875rem] pb-[13.875rem]"
     >
       <div className="
       BANNER-DESCRIPTION
-      max-w-[35rem]"
+      w-[35rem]"
       >
         <h1 className="
         HEADLINE
-        text-[5rem] font-bold tracking-[-0.03em] text-neutralVeryDarkBlue"
+        text-[5rem]/[1.163em] font-bold text-neutralVeryDarkBlue
+        tracking-[-0.03em]"
         >
           More than just shorter links
         </h1>
@@ -36,6 +42,17 @@ const WelcomeBanner = () => {
         >
           Get Started
         </button>
+      </div>
+      <div className="
+      BANNER-ILLUSTRATION
+      relative
+      -top-[4.063rem]"
+      >
+        <img
+          src={workingIllustration}
+          alt="A two dimensional illustration of a person sitting on a chair while typing on a keyboard while looking to a computer monitor on a table with a pot of plant on the left side."
+          className="absolute min-w-[733px] min-h-[482px]"
+        />
       </div>
     </section>
   );
