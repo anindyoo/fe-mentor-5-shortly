bc1a7ece4771e6375a03c04e23e55467e53c485f
anindyoo <anindyo0216@gmail.com>
2024-07-11 11:41:47 +0700

    feat: implement react-router into app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import Main from './layouts/Main';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
+  const routes = [
+    {
+      element: <Main />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+      ],
+    },
+  ];
+
+  const router = createHashRouter(routes);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 export default App;
