4dfcdd9395f1a8bfecaeea78d980729929ac9495
anindyoo <anindyo0216@gmail.com>
2024-07-11 14:30:29 +0700

    feat: add screens config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    screens: {
+      '3xs': '320px',
+      '2xs': '375px',
+      xs: '425px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {},
   },
   plugins: [],
