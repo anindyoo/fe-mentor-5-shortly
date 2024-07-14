6b678222f077a427c64decbf2e4da076b81671f8
anindyoo <anindyo0216@gmail.com>
2024-07-14 22:28:06 +0700

    feat: add shortened link list content.

--- src/components/LinkShortener/ShortenedLinkList.jsx
+++ src/components/LinkShortener/ShortenedLinkList.jsx
@@ -1,5 +1,50 @@
-const ShortenedLinkList = () => {
+const ShortenedLinkList = (props) => {
+  const {
+    shortenedLinkData,
+  } = props;
 
+  console.log(shortenedLinkData);
+
+  return (
+    <li className="
+    SHORTLINK-LIST-ITEM
+    flex flex-row justify-between items-center
+    h-[4.5rem]
+    px-8
+    rounded-md
+    bg-white"
+    >
+      <div className="
+      LONG-URL
+      text-xl font-medium text-neutralVeryDarkBlue"
+      >
+        {shortenedLinkData.longUrl}
+      </div>
+      <div className="
+      SHORT-URL
+      flex flex-row gap-6 items-center"
+      >
+        <a
+          href={shortenedLinkData.shortUrl}
+          className="text-lg font-normal text-primaryCyan"
+        >
+          {shortenedLinkData.shortUrl}
+        </a>
+        <button
+          type="button"
+          className="
+          COPY-SHORT-URL-BUTTON
+          h-10
+          px-8
+          rounded-md
+          text-semiBase font-bold text-white
+          bg-primaryCyan"
+        >
+          Copy
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default ShortenedLinkList;
