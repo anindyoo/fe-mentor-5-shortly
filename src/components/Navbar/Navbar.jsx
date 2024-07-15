25d87c67d3af5848635bdb480f932893b8274141
anindyoo <anindyo0216@gmail.com>
2024-07-15 16:49:44 +0700

    feat: set navbar to be fixed positioned.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,55 +9,62 @@ const Navbar = () => {
   return (
     <nav className="
     NAVBAR
-    SECTION-CENTER
-    flex flex-row justify-between items-center
-    h-[8.5rem]
-    text-semiBase font-bold text-neutralGrayishViolet"
+    fixed z-50
+    w-full
+    text-semiBase font-bold text-neutralGrayishViolet
+    bg-white"
     >
       <div className="
-      NAV-LINKS
-      flex flex-row gap-11 items-center"
+      NAVBAR-CONTAINER
+      SECTION-CENTER
+      flex flex-row justify-between items-center
+      h-[8.5rem]"
       >
-        <Link
-          to="/"
+        <div className="
+        NAV-LINKS
+        flex flex-row gap-11 items-center"
         >
-          <img src={logo} alt="Shortly logo." />
-        </Link>
-        <ul className="
-        NAV-LINKS-LIST
-        flex flex-row gap-[1.875rem]"
+          <Link
+            to="/"
+          >
+            <img src={logo} alt="Shortly logo." />
+          </Link>
+          <ul className="
+          NAV-LINKS-LIST
+          flex flex-row gap-[1.875rem]"
+          >
+            {navLinks.map((navLink) => (
+              <li key={navLink}>
+                <Link
+                  to="/"
+                >
+                  {navLink}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="
+        NAV-AUTH
+        flex flex-row gap-9"
         >
-          {navLinks.map((navLink) => (
-            <li key={navLink}>
-              <Link
-                to="/"
-              >
-                {navLink}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="
-      NAV-AUTH
-      flex flex-row gap-9"
-      >
-        <button
-          type="button"
-        >
-          Login
-        </button>
-        <button
-          type="button"
-          className="
-          SIGN-UP-BTN
-          bg-primaryCyan
-          w-[6.563rem] h-10
-          rounded-full
-          text-white"
-        >
-          Sign Up
-        </button>
+          <button
+            type="button"
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            className="
+            SIGN-UP-BTN
+            bg-primaryCyan
+            w-[6.563rem] h-10
+            rounded-full
+            text-white"
+          >
+            Sign Up
+          </button>
+        </div>
       </div>
     </nav>
   );
