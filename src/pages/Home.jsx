0acdbe65269099d2a8e3198622884dcd9b8d8014
anindyoo <anindyo0216@gmail.com>
2024-07-16 15:42:53 +0700

    feat: remove console.log & restructure the code.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,14 @@ import BenefitsBanner from '../components/HomeComponents/BenefitsBanner';
 import LinkShortener from '../components/HomeComponents/LinkShortener';
 import WelcomeBanner from '../components/HomeComponents/WelcomeBanner';
 
-const Home = () => {
-  console.log();
-
-  return (
-    <div className="
+const Home = () => (
+  <div className="
     HOME-PAGE"
-    >
-      <WelcomeBanner />
-      <LinkShortener />
-      <BenefitsBanner />
-    </div>
-  );
-};
+  >
+    <WelcomeBanner />
+    <LinkShortener />
+    <BenefitsBanner />
+  </div>
+);
 
 export default Home;
